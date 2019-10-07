7a0b89f0a2092aa408c9af28ceeade495dd77bee
Ericfvku <37081684+Ericfvku@users.noreply.github.com>
2019-10-06 22:15:14 -0400

    Validacion de Inputs agregada al formulario de Login

--- src/Componentes/FormularioLogin.js
+++ src/Componentes/FormularioLogin.js
@@ -1,11 +1,16 @@
 import React from "react";
 import axios from "axios";
+
+const initialState = {
+  username: "",
+  password: "",
+  rol: "",
+  usernameError: "",
+  passwordError: ""
+};
+
 class FormularioLogin extends React.Component {
-  state = {
-    username: "",
-    password: "",
-    rol: ""
-  };
+  state = initialState;
   change = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -17,52 +22,81 @@ class FormularioLogin extends React.Component {
     const rols = datos[0].rol_id;
     console.log(rols);
   }
+
+  validar = () => {
+    let usernameError = "";
+    let passwordError = "";
+
+    if (!this.state.password) {
+      passwordError = "Este campo no puede estar vacio";
+    }
+    if (!this.state.username) {
+      usernameError = "Este campo no puede estar vacio";
+    }
+    if (usernameError || passwordError) {
+      this.setState({ passwordError, usernameError });
+      return false;
+    }
+    return true;
+  };
   submitHandler = e => {
     e.preventDefault();
-    axios
-      .post("https://localhost:44356/api/myLogin", {
-        username: this.state.username,
-        password: this.state.password
-      })
-      .then(res => localStorage.setItem("data", res.data))
-      .then(this.getdata)
-      .then(() => {
-        if (localStorage.getItem("rol") == "2") {
-          this.props.history.push("/");
-        }
-        if (localStorage.getItem("rol") == "1") {
-          this.props.history.push("/admin");
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    console.log(this.state);
+    const esValido = this.validar();
+    if (esValido) {
+      axios
+        .post("https://localhost:44356/api/myLogin", {
+          username: this.state.username,
+          password: this.state.password
+        })
+        .then(res => localStorage.setItem("data", res.data))
+        .then(this.getdata)
+        .then(() => {
+          if (localStorage.getItem("rol") == "2") {
+            this.props.history.push("/");
+          }
+          if (localStorage.getItem("rol") == "1") {
+            this.props.history.push("/admin");
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      this.setState(initialState);
+    }
   };
-  loginHandler() {}
+
   render() {
     if (localStorage.getItem("rol") == null) {
       return (
         <div>
           INICIAR SESION
           <form onSubmit={this.submitHandler}>
+            <div>
+              <br></br>
+              <input
+                type='text'
+                name='username'
+                placeholder='Nombre de Usuario'
+                value={this.state.username}
+                onChange={e => this.change(e)}
+              />
+            </div>
+            <div style={{ fontSize: 12, color: "red" }}>
+              {this.state.usernameError}
+            </div>
             <br></br>
-            <input
-              type='text'
-              name='username'
-              placeholder='Nombre de Usuario'
-              value={this.state.username}
-              onChange={e => this.change(e)}
-            />
-            <br></br>
-            <input
-              type='password'
-              name='password'
-              placeholder='Password'
-              value={this.state.password}
-              onChange={e => this.change(e)}
-            ></input>
-            <br></br>
+            <div>
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                value={this.state.password}
+                onChange={e => this.change(e)}
+              ></input>
+            </div>
+            <div style={{ fontSize: 12, color: "red" }}>
+              {this.state.passwordError}
+            </div>
             <br></br>
             <br></br>
             <button type='submit'>Iniciar Sesión</button>
