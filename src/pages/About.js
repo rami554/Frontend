ea6f1d32e3e1dd3f5b2201c5987bcd4d29bd9fa8
rami554 <menendez.ticona.ramiro@gmail.com>
2019-10-17 06:19:10 -0400

    mejora de vistas

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import img4 from "./img4.png";
 class About extends React.Component {
   render() {
     return (
-      <div>
+      <div class='row justify-content-center'>
         <h1>Acerca de Nosotros</h1>
         <div>Somos una nueva empresa que te proporciona entradas para ver las mejores peliculas de los tiempos</div>
         <div>Te Proporcionamos diversas funciones y todo a traves de un tiquet electronico</div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,22 +1,37 @@
 import React from "react";
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
 
 class Contact extends React.Component {
   render() {
     return (
-      <div>
+      <Container className="Contactos">
         <h1>Contact</h1>
-        <form>
-          <div>
-            Nombre:
-            <input type='text' name='name' />
-          </div>
-          <div>
-            Comentario:
-            <input type='text' name='coment' />
-          </div>
-          <input type='submit' value='Enviar' />
+        <form className="form">
+           <Col>
+            <FormGroup>
+              <Label>Nombre</Label>
+              <Input
+                type="text"
+                name="name"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Comentario</Label>
+              <Input
+                type="textarea"
+                name="comentario"
+              />
+            </FormGroup>
+          </Col>
+          <Button type="submit">Enviar</Button>
         </form>
-      </div>
+      </Container>
     );
   }
 }
